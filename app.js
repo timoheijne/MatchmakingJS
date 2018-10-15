7531df7cd0df3f037dd82f297ef46db99cb96f91
Timo Heijne <me@timoheijne.nl>
2018-10-15 22:01:52 +0200

    Setup basic logging & testing environment

---
+++ app.js
@@ -0,0 +1,9 @@
+const logger = require('./logger');
+
+logger.info("Initializing Matchmaking Service")
+logger.info("test")
+
+// Spawn Child Processes for:
+// 1. Web Server for real-time tracking
+// 2. Matchmaking Server
+// 3. Server Spawner 
---
+++ logger.js
@@ -0,0 +1,39 @@
+const pino = require('pino')
+const fs = require('fs');
+const childProcess = require('child_process');
+const stream = require('stream');
+
+// Environment variables
+const cwd = process.cwd();
+const { env } = process;
+const logPath = `${__dirname}/logs`;
+
+// Create a stream where the logs will be written
+const logThrough = new stream.PassThrough();
+const logger = pino({ name: 'Matchmaking Service' }, logThrough);
+
+if (!fs.existsSync(logPath)) {
+    fs.mkdirSync(logPath)
+}
+
+// Log to multiple files using a separate process
+const child = childProcess.spawn(process.execPath, [
+    require.resolve('pino-tee'),
+    'warn', `${logPath}/warn.log`,
+    'error', `${logPath}/error.log`,
+    'fatal', `${logPath}/fatal.log`,
+    'info', `${logPath}/info.log`,
+    'debug', `${logPath}/debug.log`,
+], { cwd, env });
+
+logThrough.pipe(child.stdin);
+logThrough.on('data', (data) => {
+    console.log(JSON.parse(data.toString().trim()).msg);
+})
+
+const pretty = pino.pretty()
+pretty.pipe(child.stdout)
+
+logger.level = process.env.LOG_LEVEL || "debug";
+
+module.exports = logger;
