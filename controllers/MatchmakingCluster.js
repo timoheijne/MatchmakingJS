e613b40103b80b134cda817f23d685846b413d48
Timo Heijne <me@timoheijne.nl>
2018-11-01 00:42:34 +0100

    It's not equiped to receive the important data

--- controllers/MatchmakingCluster.js
+++ controllers/MatchmakingCluster.js
@@ -1,11 +1,31 @@
 const clients = [];
 const lobbies = {};
 
+const loop = true;
+
+let matchmakingStep;
+
 /* Expected events:
  * matchmaking.start: Expect: clients input
  *
  */
 process.on('message', message => {
     const event = message.event;
-    console.log(message)
+
+    if(event ==  "matchmaking.start") {
+        clients.push(message.clientData);
+
+        initMatchmaking();
+    }
+    
+    console.log(clients)
 })
+
+function initMatchmaking() {
+    if(matchmakingStep) return; // Its already running
+
+    matchmakingStep = setInterval(() => {
+        // This is a matchmaking step.
+        console.log('dab')
+    }, 500); // We don't need a while true loop here, Search for a match every .5 of a second is more than enough
+}
